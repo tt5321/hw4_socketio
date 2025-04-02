343ac3f3b030ee01c8d61d3715bb5fc829038c69
tt5321 <tiantian5321.k@outlook.com>
2025-04-01 18:26:01 -0700

    Add creative feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,28 @@ import "./App.css";
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentOpt, setCurrentOpt] = useState("All");
 
   const handleSendMessage = (e) => {
     e.preventDefault()
     const input = e.target.msg;
-    const message = "User " + socket.id + ": " + input.value; // Add the socket ID to the message
-    socket.emit("message", message);
+    const message = input.value;
+    socket.emit("message", message, currentOpt); // Send the message and the selected user to the server
     input.value = "";
   };
 
   useEffect(() => {
     socket.on("message", (msg) => {
       const time = new Date().toLocaleTimeString();
-      console.log("Message received:", msg + " [" + time +"]"); // Log the message to the console
-      const full_msg = " [" + time +"] " + msg; // Add the timestamp to the message
-      setMessages((prevMessages) => [...prevMessages, full_msg]);
+      const fullMsg = msg + " [" + time + "]"; // Add the timestamp to the message
+      console.log("Message received:", fullMsg); // Log the message to the console
+      setMessages((prevMessages) => [...prevMessages, fullMsg]);
+    });
+
+    socket.on("users", (users) => {
+      console.log("Users connected:", users); // Log the users to the console
+      setUsers(users);
     });
   }, []);
 
@@ -31,11 +38,25 @@ function App() {
           <li key={index}>{msg}</li>
         ))}
       </ul>
-      <h3>Send Message:</h3>
+      <h2>Send Message:</h2>
       <form id="form" onSubmit={handleSendMessage} >
+        <select id="select" onChange={(e) => setCurrentOpt(e.target.value)}>
+          <option value="All">All</option>
+          {users.filter(usr => usr !== socket.id).map((user, index) => (
+            <option key={index} value={user}>{user}</option>
+          ))}
+        </select>
         <input id="input" autocomplete="off" name="msg" />
         <button type="submit">Send</button>
       </form>
+      <br />
+      <text>* I am: {socket.id}</text>
+      <h2>Online Users</h2>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
     </>
   )
 }
--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 import express from "express";
 import { createServer } from 'node:http';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
 const ioServer = new Server(server);
+let users = [];
 app.use(express.static("frontend/dist"));
 
 ioServer.on('connection', (socket) => {
     console.log('A user connected', socket.id);
+    users.push(socket.id); // Add the socket ID to the users array
+    ioServer.emit('users', users); // Emit the updated users array to all clients
 
-    socket.on('message', (message) => {
+    socket.on('message', (message, currentOpt) => {
         const time = new Date().toLocaleTimeString();
-        console.log('Message from client -', message, " [", time, "]"); // Log the message with timestamp to the console
-        ioServer.emit('message', message);
+        console.log('Message from client ' + socket.id + "to" + currentOpt + ": " + message + " [" + time + "]"); // Log the message with timestamp to the console
+        const msgWithID = "User " + socket.id + ": " + message; // Add the usern socket ID to the message
+        if (currentOpt === "All") {
+            ioServer.emit('message', msgWithID); // Emit the message to all clients
+        }
+        else {
+            socket.to(currentOpt).emit('message', msgWithID + " (private)"); // Emit the message to the selected user
+            socket.emit('message', msgWithID + " (private -> " + currentOpt + ")"); // Emit the message to the sender as well
+        }
     });
     
     socket.on('disconnect', () => {
         console.log('A user disconnected', socket.id);
+        users = users.filter(usr => usr !== socket.id); // Remove the socket ID from the users array
+        ioServer.emit('users', users); // Emit the updated users array to all clients
     });
 });
 
